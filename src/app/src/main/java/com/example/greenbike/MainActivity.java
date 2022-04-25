42669de00a007f3535863c37f6767186515d515d
PetarKTodorov <PetarKTodorov@gmail.com>
2022-04-25 20:49:59 +0200

    Update MainActivity.java

--- src/app/src/main/java/com/example/greenbike/MainActivity.java
+++ src/app/src/main/java/com/example/greenbike/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,20 +17,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.greenbike.common.Global;
+import com.example.greenbike.database.common.Constatants;
+import com.example.greenbike.database.models.user.User;
+import com.example.greenbike.database.models.user.UserRole;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtResponse;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txtResponse = (TextView)findViewById(R.id.txtResponse);
     }
 
     public void toView(View v)
