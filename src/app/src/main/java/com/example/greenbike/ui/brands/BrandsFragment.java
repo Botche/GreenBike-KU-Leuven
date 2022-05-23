757411a72112761494d715a9359e9bfe1432dc0e
Gabriel <47752724+Botche@users.noreply.github.com>
2022-05-23 18:24:20 +0200

    Remove unused view models

--- src/app/src/main/java/com/example/greenbike/ui/brands/BrandsFragment.java
+++ src/app/src/main/java/com/example/greenbike/ui/brands/BrandsFragment.java
@@ -4,36 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.greenbike.databinding.FragmentBrandsBinding;
 
 public class BrandsFragment extends Fragment {
 
-    private BrandsViewModel brandsViewModel;
     private FragmentBrandsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        brandsViewModel =
-                new ViewModelProvider(this).get(BrandsViewModel.class);
-
         binding = FragmentBrandsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        brandsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
