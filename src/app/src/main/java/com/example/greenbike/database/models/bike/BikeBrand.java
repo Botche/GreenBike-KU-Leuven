d5840a0681177ee02ad5005e10fe2169a9854ddf
Gabriel <47752724+Botche@users.noreply.github.com>
2022-05-28 11:29:37 +0200

    Remove unused imports

--- src/app/src/main/java/com/example/greenbike/database/models/bike/BikeBrand.java
+++ src/app/src/main/java/com/example/greenbike/database/models/bike/BikeBrand.java
@@ -1,7 +1,5 @@
 package com.example.greenbike.database.models.bike;
 
-import androidx.annotation.NonNull;
-
 public class BikeBrand extends BaseBike {
     public BikeBrand(String id, String name) {
         super(id, name);
--- src/app/src/main/java/com/example/greenbike/database/models/bike/BikeMaterial.java
+++ src/app/src/main/java/com/example/greenbike/database/models/bike/BikeMaterial.java
@@ -1,7 +1,5 @@
 package com.example.greenbike.database.models.bike;
 
-import java.io.Serializable;
-
 public class BikeMaterial extends BaseBike {
     public BikeMaterial(String id, String name) {
         super(id, name);
